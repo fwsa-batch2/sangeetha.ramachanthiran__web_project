7519a0ebca01c39baf1b27d36ca68d60845aa731
SangeethaRamachanthiran <sangeetha.r1409@gmail.com>
2022-01-17 17:36:29 +0530

    added products

--- assets/js/sellArts.js
+++ assets/js/sellArts.js
@@ -1,4 +1,5 @@
-let array =[];
+let array =[{imageSrc:"../assets/img/smile_headset.jpeg",artsName: "Headset",artsType: "Pencil drawing",artistsName: "Ismail",artistsPlace: "Thoothukudi",artsPrice: 500},
+{imageSrc:"../assets/img/smile_subway.jpg",artsName: "Subway Surfers",artsType: "Pencil drawing",artistsName: "Ismail",artistsPlace: "Thoothukudi",artsPrice: 500},];
 function getData(){
  
     let getLocalStorage =  localStorage.getItem("itemS");
